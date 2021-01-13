0e938e74e8cd662888a9f2f71e67008fd98dfb79
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-01-13 14:28:22 -0800

    added throwButton

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 // import functions and grab DOM elements
+const currentThrow = document.getElementById('current-throw');
+const winsTotalSpan = document.getElementById('wins-total');
+const lossesTotal = document.getElementById('losses-total');
+const totalSpan = document.getElementById('total');
+const throwRadio = document.querySelector('input[type="radio"]:checked');
 
 // initialize state
+let winsTotal = 0;
+let total = 0;
+
 
 // set event listeners to update state and DOM
+throwRadio.addEventListener('click', () => {
+    total++;
+    console.log(currentThrow, winsTotalSpan, lossesTotalSpan, totalSpan);
+});
