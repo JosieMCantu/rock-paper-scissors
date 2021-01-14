696c0f7c273e4a3e3e1c66562e64bb28fa6bbaf4
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-01-13 20:33:32 -0800

    adds compare-throws.js file

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 // import functions and grab DOM elements
 import { rockPaperScissors } from './utils.js';
-import { compareThrows } from './utils.js';
+import { compareThrows } from './compare-throws.js';
 
 const winsTotalSpan = document.getElementById('wins-total');
 const lossesTotalSpan = document.getElementById('losses-total');
 const totalSpan = document.getElementById('total');
 const throwButton = document.getElementById('throw-button');
+const currentThrow = document.getElementById('current-throw');
+const drawsSpan = document.getElementById('draws-span');
 
-// initialize state
 let winsTotal = 0;
 let total = 0;
+let draws = 0;
 
-
-// set event listeners to update state and DOM
 throwButton.addEventListener('click', () => {
     total++;
-    const computerThrow = Math.round(Math.random());
+    const computerThrow = Math.random();
     const computerRps = rockPaperScissors(computerThrow);
-    
-// Store the user's throw
-// grab the checked input, and get its value
     const throwRadio = document.querySelector('input[type="radio"]:checked');
     const userThrow = throwRadio.value; 
 
-// We compare the user's guess to the computer throw
-compareThrows(computerRps, userThrow);
-
-console.log(compareThrows);
+    if(compareThrows(computerRps, userThrow) === 'lose'){
+        currentThrow.textContent = `You lose!`;
+    }
+    if(compareThrows(computerRps, userThrow) === 'win'){
+        winsTotal++;
+        currentThrow.textContent = `You win!`;    
+    }
+    if(compareThrows(computerRps, userThrow) === 'draw'){
+        currentThrow.textContent = `Draw!`;
+    }
 resultsUpdate();
-
 });
-// We need to display the result of the the throw
+
 function resultsUpdate(){
     winsTotalSpan.textContent = winsTotal;
     lossesTotalSpan.textContent = total - winsTotal;
+    drawsSpan.textContent = draws;
     totalSpan.textContent = total;
 }
--- utils.js
+++ utils.js
@@ -7,21 +7,3 @@ export function rockPaperScissors(randomThrow){
         return 'Scissors';
     }
 }
-export function compareThrows(computerRps, userThrow){
-    
-    if (computerRps === 'Scissors' && userThrow === 'Paper'){
-        currentThrow.textContent = `You lose! You threw ${userThrow}. The correct throw was ${computerRps}.`;
-    }else if (computerRps === 'Rock' && userThrow === 'Scissors'){
-        currentThrow.textContent = `You lose! You threw ${userThrow}. The correct throw was ${computerRps}.`;
-    }else if (computerRps === 'Paper' && userThrow === 'Rock'){
-        currentThrow.textContent = `You lose! You threw ${userThrow}. The correct throw was ${computerRps}.`;
-    }else if (computerRps === 'Scissors' && userThrow === 'Rock'){
-        currentThrow.textContent = `You win! You threw ${userThrow}. The correct throw was ${computerRps}.`;
-    }else if (computerRps === 'Rock' && userThrow === 'Paper'){
-        currentThrow.textContent = `You win! You threw ${userThrow}. The correct throw was ${computerRps}.`;
-    }else if (computerRps === 'Paper' && userThrow === 'Scissors'){
-        currentThrow.textContent = `You win! You threw ${userThrow}. The correct throw was ${computerRps}.`;
-    } else if (computerRps === userThrow){
-        currentThrow.textContent = `Draw! You threw ${userThrow}. The correct throw was ${computerRps}.`;
-    }
-}
